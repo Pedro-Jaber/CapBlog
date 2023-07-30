fb31756cd109af57583a49f1975f31163f20d4f0
Pedro Jaber <pedrojaberc@gmail.com>
2023-07-30 15:07:49 -0300

    aula 4 e aula 5

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const { connectDB } = require('./server/config/db')
+
 
 const app = express();
 const PORT = 8081;
+
+// Connect to DB
+connectDB();
 
 // Public
 app.use(express.static('public'));
@@ -22,6 +27,7 @@ app.use('/', require('./server/routes/main'));
 
 
 app.listen(PORT, () => {
+    console.log("===========================")
     console.log("Server:\x1b[92m Online \x1b[0m");
     console.log("Port: " + PORT)
     console.log(`link: http://localhost:${PORT}`);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,100 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
+
+/**
+ * GET /
+ * HOME
+ */
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "CapBlog",
+      body: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    const postPerPage = 5;
+    let page = req.query.page || 1;
+
+    if (page <= 0) {
+      page = 1;
+    }
+
+    //const data = await Post.find();
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(postPerPage * page - postPerPage)
+      .limit(postPerPage)
+      .exec();
+
+    const countOfPosts = await Post.count(); // Conta a quantidade de Posts
+    const nextPage = parseInt(page) + 1; // Adiquire o numero da prox página para ser usado no botão
+    const totalOfPages = Math.ceil(countOfPosts / postPerPage); // conta a quantidade de páginas 
+    const hasNextPage = nextPage <= totalOfPages; // Verifica se ha uma nova página (valores: true || false)
 
 
-// Routes
-router.get('/', (req, res) => {
-  res.render('index');
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/about', (req, res) => {
-  res.render('about');
+router.get("/about", (req, res) => {
+  res.render("about");
 });
+//
+//
+//
+//
+function insertPostData() {
+  Post.insertMany([
+    {
+      title: "Building APIs with Node.js",
+      body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js",
+    },
+    {
+      title: "Deployment of Node.js applications",
+      body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments...",
+    },
+    {
+      title: "Authentication and Authorization in Node.js",
+      body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries.",
+    },
+    {
+      title: "Understand how to work with MongoDB and Mongoose",
+      body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications.",
+    },
+    {
+      title: "build real-time, event-driven applications in Node.js",
+      body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js.",
+    },
+    {
+      title: "Discover how to use Express.js",
+      body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications.",
+    },
+    {
+      title: "Asynchronous Programming with Node.js",
+      body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations.",
+    },
+    {
+      title: "Learn the basics of Node.js and its architecture",
+      body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers.",
+    },
+    {
+      title: "NodeJs Limiting Network Traffic",
+      body: "Learn how to limit netowrk traffic.",
+    },
+    {
+      title: "Learn Morgan - HTTP Request logger for NodeJs",
+      body: "Learn Morgan.",
+    },
+  ]);
+}
 
+//insertPostData();
 
-
-module.exports = router
+module.exports = router;
